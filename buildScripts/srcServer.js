c3c62e31ddb39ca664bdc7536637b5a745a81df8
gary <ggrumbley@ucsd.edu>
2017-06-25 21:10:58 -0700

    Add webpack config

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import path from 'path';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const app = express();
+
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
